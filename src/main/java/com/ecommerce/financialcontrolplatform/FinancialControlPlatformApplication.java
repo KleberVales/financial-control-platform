bd491da90a927c3c2f0891f212d35017c8115b6f
Kleber Vales <klebervales.dev@gmail.com>
2026-02-14 11:18:40 -0300

    feat: Daily Dev Log

---
+++ src/main/java/com/ecommerce/financialcontrolplatform/FinancialControlPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.financialcontrolplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControlPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialControlPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/financialcontrolplatform/FinancialControlPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.financialcontrolplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControlPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
